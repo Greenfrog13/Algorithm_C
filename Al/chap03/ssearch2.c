81fe8fc61d8d0969f5b27aa150ae2604c5a65c47
JiwonJeong <50576758+Greenfrog13@users.noreply.github.com>
2021-05-15 13:24:29 +0900

    실습 3-2
    요소의 개수가 n인 배열 a에서 key와 일치하는 요소를 선형 검색 (버전 2: for문)

---
+++ Al/chap03/ssearch2.c
@@ -0,0 +1,8 @@
+// 요소의 개수가 n인 배열 a에서 key와 일치하는 요소를 선형 검색 (버전 2: for 문)
+int search(const int a[], int n, int key){
+	int i;
+	for(i=0;i<n;i++)
+		if(a[i] == key)
+			return i; //검색 성공
+	return -1; //검색 실패
+} 
