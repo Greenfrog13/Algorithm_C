05582b90c4452f5fa9db85217ca4e14f20148f58
JiwonJeong <50576758+Greenfrog13@users.noreply.github.com>
2021-05-18 17:06:02 +0900

    실습 6-4
    단순 선택 정렬

---
+++ Al/chap06/selection.c
@@ -0,0 +1,11 @@
+//단순 선택 정렬 
+void selection(int a[], int n){
+	int i, j;
+	for(i=0;i<n-1;i++){
+		int min = i;
+		for(j=1+1;j<n;j++)
+			if(a[j] < a[min])
+				min = j;
+		swap(int, a[i], a[min]);
+	}
+}
